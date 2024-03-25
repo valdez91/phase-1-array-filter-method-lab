3d43f01eabb0038ecce5a9efce074e203dff52b2
Victor Kemboi <vickemtoo97@gmail.com>
2024-03-25 15:05:04 +0300

    array-filters

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+ findMatching =(name, drivers) =>{
+    return name.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === drivers.toLowerCase()
+    );
+  }
+  
+  fuzzyMatch =(name, testString) =>{
+    return name.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  matchName =(name, driveName) =>{
+    return name.filter((record) => record.name === driveName);
+  }
